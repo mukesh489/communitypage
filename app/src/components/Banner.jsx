b547cb8a0f692c0fdb8989529ff82a199a0afe77
mukesh yadav <83478699+mukesh489@users.noreply.github.com>
2024-07-23 23:02:09 +0530

    Update Banner.jsx

--- app/src/components/Banner.jsx
+++ app/src/components/Banner.jsx
@@ -4,19 +4,19 @@ import banner from '../asset/banner.jpg'; // Make sure the path is correct
 const Banner = () => {
   return (
     <>
-      <div className='flex flex-col items-center m-2'>
-        <h1 className='text-2xl text-white'> Member Community</h1>
-       
+      <div className='flex flex-col items-center m-4 lg:m-8 '>
+        <h1 className='text-3xl lg:text-5xl text-white'>Member Community</h1>
       </div>
-      <div className='p-4'>
-        <img src={banner} alt="Banner" />
+      <div className='p-4 lg:p-12 lg:m-12'>
+        <img src={banner} alt="Banner" className='w-full h-auto lg:max-w-6xl mx-auto' />
       </div>
-      <div className='flex flex-col items-center m-2' >
-        <p  className='text-xl text-white m-4' > Multiply your earning by participating more in the community!</p>
-       
+      <div className='flex flex-col items-center m-4 lg:m-8'>
+        <p className='text-lg lg:text-2xl text-white'>
+          Multiply your earning by participating more in the community!
+        </p>
       </div>
     </>
   );
-}
+};
 
 export default Banner;
